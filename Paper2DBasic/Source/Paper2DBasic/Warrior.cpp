cee665ef5bbf7cd6176250743754082322f9aacc
RYanIndieDev <AtCloudStudio@gmail.com>
2023-10-06 21:59:08 -0400

    Ep.03

--- Paper2DBasic/Source/Paper2DBasic/Warrior.cpp
+++ Paper2DBasic/Source/Paper2DBasic/Warrior.cpp
@@ -8,11 +8,14 @@
 
 AWarrior::AWarrior()
 {
+	JumpMaxHoldTime = 0.3f;
+	
 	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera Boom"));
 	SpringArmComponent->SetupAttachment(RootComponent);
 	SpringArmComponent->TargetArmLength = 900.0f;
 	SpringArmComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 40.0f));
 	SpringArmComponent->SetRelativeRotation(FRotator(0.0f, -90.0f,  0.0f));
+	SpringArmComponent->bInheritYaw = false;
 
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	CameraComponent->SetupAttachment(SpringArmComponent);
@@ -20,14 +23,129 @@ AWarrior::AWarrior()
 	GetArrowComponent()->SetHiddenInGame(false);
 	
 	GetSprite()->SetCastShadow(true);
+	GetSprite()->SetRelativeLocation(DefaultSpriteOffset);
+	GetSprite()->SetRelativeScale3D(FVector(5.0f));
+
+	GetCharacterMovement()->GetNavAgentPropertiesRef().bCanCrouch = true;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 10000.0f, 0.0f);
+	GetCharacterMovement()->CrouchedHalfHeight = 50.0f;
+	GetCharacterMovement()->GravityScale = 4.0f;
+	GetCharacterMovement()->JumpZVelocity = 800.0f;
+	GetCharacterMovement()->AirControl = 0.9f;
 }
 
-bool AWarrior::IsRunning() const
+void AWarrior::Tick(float DeltaSeconds)
 {
-	return GetCharacterMovement()->IsMovingOnGround() && GetVelocity().X != 0.0f;
+	Super::Tick(DeltaSeconds);
+
+	if (IsSliding)
+	{
+		AddMovementInput(GetActorForwardVector());
+	}
+}
+
+void AWarrior::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CrouchedSpriteOffset = FVector(DefaultSpriteOffset.X, DefaultSpriteOffset.Y, CrouchedSpriteHeight);
+
+	LandedDelegate.AddDynamic(this, &AWarrior::OnCharacterLanded);
 }
 
 void AWarrior::Move(const float InputActionValue)
 {
+	if (bIsCrouched)
+	{
+		if (InputActionValue < 0.0f)
+		{
+			SetActorRotation(FRotator(0.0f, 180.0f, 0.0f));
+		}
+		else if (InputActionValue > 0.0f)
+		{
+			SetActorRotation(FRotator(0.0f));
+		}
+		
+		return;
+	}
+	
 	AddMovementInput(FVector::ForwardVector, InputActionValue);
+	SetMoveInputValue(InputActionValue);
+}
+
+void AWarrior::Crouch(bool bClientSimulation)
+{
+	Super::Crouch(bClientSimulation);
+	GetSprite()->SetRelativeLocation(CrouchedSpriteOffset);
+}
+
+void AWarrior::UnCrouch(bool bClientSimulation)
+{
+	if (IsSliding || IsWallAbove())
+	{
+		return;
+	}
+	
+	Super::UnCrouch(bClientSimulation);
+	GetSprite()->SetRelativeLocation(DefaultSpriteOffset);
+}
+
+void AWarrior::Slide()
+{
+	IsSliding = true;
+
+	FTimerHandle SlideTimerHandle;
+	GetWorldTimerManager().SetTimer(SlideTimerHandle, this, &AWarrior::StopSlide, SlideDuration);
+}
+
+void AWarrior::StopSlide()
+{
+	IsSliding = false;
+
+	if (bIsCrouched) return; // TODO: DEBUG Crouch Input
+		
+	UnCrouch();
+}
+
+void AWarrior::OnJumpInput()
+{
+	if (IsSliding) return;
+		
+	if (bIsCrouched)
+	{
+		Slide();
+
+		return;
+	}
+
+	Jump();
+	IsJumping = true;
+}
+
+bool AWarrior::IsGrounded() const
+{
+	return GetCharacterMovement()->IsMovingOnGround();
+}
+
+void AWarrior::OnCharacterLanded(const FHitResult& Hit)
+{
+	IsJumping = false;
+}
+
+bool AWarrior::IsWallAbove() const
+{
+	FHitResult HitResult;
+	FCollisionQueryParams Params;
+
+	Params.AddIgnoredActor(this);
+	
+	GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		GetActorLocation(),
+		GetActorLocation() + FVector(0.0f, 0.0f, 100.0f),
+		ECC_Visibility,
+		Params);
+
+	return HitResult.bBlockingHit && HitResult.GetActor()->ActorHasTag(WallTag);
 }
--- Paper2DBasic/Source/Paper2DBasic/Warrior.h
+++ Paper2DBasic/Source/Paper2DBasic/Warrior.h
@@ -15,16 +15,66 @@ class PAPER2DBASIC_API AWarrior : public APaperZDCharacter
 public:
 	AWarrior();
 
-	UFUNCTION(BlueprintPure, Category = "Animation Parameters")
-	bool IsRunning() const;
+	virtual void Tick(float DeltaSeconds) override;
+
+protected:
+	virtual void BeginPlay() override;
+	
+	bool IsWallAbove() const;
 
 private:
-	UPROPERTY(VisibleAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(VisibleAnywhere, Category = "Components", meta=(AllowPrivateAccess = "true"))
 	USpringArmComponent* SpringArmComponent;
 
-	UPROPERTY(VisibleAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(VisibleAnywhere, Category = "Components", meta=(AllowPrivateAccess = "true"))
 	UCameraComponent* CameraComponent;
 
+	UPROPERTY(BlueprintReadOnly, Category = "Wall Check", meta=(AllowPrivateAccess = "true"))
+	FName WallTag =  FName(TEXT("Wall"));
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Animation Parameters", meta=(AllowPrivateAccess = "true"))
+	bool IsSliding;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation Parameters", meta=(AllowPrivateAccess = "true"))
+	bool IsJumping;
+
+	UPROPERTY(EditAnywhere, Category = "Movement|Slide", meta=(AllowPrivateAccess = "true"))
+	float SlideDuration = 0.5f;
+
+	UPROPERTY(BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	float MoveInputValue;
+
+	UPROPERTY(EditAnywhere, Category = "Movement|Crouch", meta=(AllowPrivateAccess = "true"))
+	float CrouchedSpriteHeight = 50.0f;
+
+	const FVector DefaultSpriteOffset = FVector(25.0f, 0.0f, 18.0f);
+	
+	FVector CrouchedSpriteOffset;
+	
+	UFUNCTION(BlueprintPure, Category = "Animation Parameters")
+	FORCEINLINE bool IsRunning() const { return IsGrounded() && MoveInputValue != 0.0f; }
+
+	UFUNCTION(BlueprintPure, Category = "Animation Parameters")
+	FORCEINLINE bool IsFalling() const { return !IsGrounded() && GetVelocity().Z <= 0.0f; }
+
+	UFUNCTION(BlueprintPure, Category = "Animation Parameters")
+	bool IsGrounded() const;
+	
 	UFUNCTION(BlueprintCallable, Category = "Movement")
 	void Move(const float InputActionValue);
+
+	virtual void Crouch(bool bClientSimulation = false) override;
+	virtual void UnCrouch(bool bClientSimulation = false) override;
+
+	void Slide();
+	void StopSlide();
+
+	UFUNCTION(BlueprintCallable, Category=Movement)
+	void OnJumpInput();
+
+	UFUNCTION(BlueprintCallable, Category=Movement)
+	FORCEINLINE void SetMoveInputValue(const float InValue) { MoveInputValue = InValue; }
+
+	UFUNCTION()
+	void OnCharacterLanded(const FHitResult& Hit);
 };
